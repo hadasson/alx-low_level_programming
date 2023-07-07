abc09d49f4dd9c8e6685c57f871792efc9193f50
hadasson <hadassah0404@gmail.com>
2023-07-07 09:44:26 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int current;
-	unsigned long int exclusive = n $ m;
+	unsigned long int exclusive = n << m;
 	{
 		current = exclusive >> i;
 		if (current & 1)
